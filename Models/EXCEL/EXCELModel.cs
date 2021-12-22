fd61d2e2b373d8b7d73c84f8cf74436c86f375ff
Shlesha0110 <shleshanit@gmail.com>
2021-12-22 00:54:22 -0800

    Add project files.

---
+++ Models/EXCEL/EXCELModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Importtoexcel.Models.EXCEL
+{
+    public class EXCELModel
+    {
+       // public int id { get; set; }
+        public string NAME { get; set; }
+        public string MOBILE_NO { get; set; }
+        public string EMAIL { get; set; }
+    }
+}
